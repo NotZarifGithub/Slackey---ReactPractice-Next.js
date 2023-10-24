be322875f7b9a1ea7cc652d693494942a1bdd16b
NotZarifGithub <flacko.programming@gmail.com>
2023-10-24 20:00:56 +0800

    mainTitle.jsx added

---
+++ src/components/common/MainTitle.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const MainTitle = ({mainTitle, title}) => {
+  return (
+    <section>
+      <div>
+
+        {/* title */}
+        <div className='flex flex-col p-[10px] capitalize'>
+
+          <div>
+            <h2 className='font-medium text-black/60'>
+              {title} / {mainTitle}
+            </h2>
+          </div>
+          <div>
+            <h1 className='text-4xl font-bold'>
+              {mainTitle}
+            </h1>
+          </div>
+        </div>
+        </div>
+    </section>
+  )
+}
+
+export default MainTitle
