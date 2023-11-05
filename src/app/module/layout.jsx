6044c5e89644ea382db4ff0ee619f329942f29ff
NotZarifGithub <flacko.programming@gmail.com>
2023-11-05 17:01:41 +0800

    fix:sidebar removed if screen size small

--- src/app/module/layout.jsx
+++ src/app/module/layout.jsx
@@ -3,9 +3,10 @@ import Sidebar from "@/components/common/Sidebar"
 export default function DashboardLayout({
   children, // will be a page or nested layout
 }) {
+  
   return (
     <section className="flex py-[30px]">
-      <div>
+      <div className="hidden md:inline-block">
         <Sidebar />
       </div>
       <div className="w-full ">
