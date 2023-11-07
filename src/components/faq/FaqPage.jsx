7b93aab149260c7589c0943daa17e948c848c605
NotZarifGithub <flacko.programming@gmail.com>
2023-11-07 16:45:29 +0800

    feat: added animation on faq page

--- src/components/faq/FaqPage.jsx
+++ src/components/faq/FaqPage.jsx
@@ -3,6 +3,7 @@
 import { motion } from "framer-motion"
 import { useState } from "react"
 import {MdOutlineKeyboardArrowDown} from "react-icons/md"
+import AnimatedSections from "../ui/animation/AnimatedSections"
 
 const FaqPage = () => {
 
@@ -39,63 +40,65 @@ const FaqPage = () => {
   };
 
   return (
-    <section className='h-[calc(100vh-84px)] px-[20px] max-w-[800px] mx-auto pt-[60px] md:py-[60px]'>
-      <div className='flex flex-col items-center h-screen gap-5'>
-        
-        {/* title */}
-        <div className="text-center py-[10px] md:py-[30px]">
-          <h1 className='text-3xl font-bold capitalize md:text-5xl'>
-            frequently asked questions
-          </h1>
-        </div>
+    <AnimatedSections > 
+      <section className='h-[calc(100vh-84px)] px-[20px] max-w-[800px] mx-auto pt-[60px] md:py-[60px]'>
+        <div className='flex flex-col items-center h-screen gap-5'>
+          
+          {/* title */}
+          <div className="text-center py-[10px] md:py-[30px]">
+            <h1 className='text-3xl font-bold capitalize md:text-5xl'>
+              frequently asked questions
+            </h1>
+          </div>
 
-        {faqData.map((item, index) => (
+          {faqData.map((item, index) => (
 
-          <button
-            key={index}
-            className="w-full border-b border-black/30 py-[20px] z-10"
-            onClick={() => handleItemClick(index)}
-          >
-
-            {/* question */}
-            <motion.div
-              className="flex items-center justify-between text-xl font-semibold md:text-2xl text-start"
+            <button
+              key={index}
+              className="w-full border-b border-black/30 py-[20px] z-10"
+              onClick={() => handleItemClick(index)}
             >
+
+              {/* question */}
               <motion.div
-                animate={{ color: activeItems[index] ? "#974ae8" : "#000000" }}
-                transition={{ duration: 0.6 }}
+                className="flex items-center justify-between text-xl font-semibold md:text-2xl text-start"
               >
-                {item.question}
+                <motion.div
+                  animate={{ color: activeItems[index] ? "#974ae8" : "#000000" }}
+                  transition={{ duration: 0.6 }}
+                >
+                  {item.question}
+                </motion.div>
+                <motion.div
+                  className="text-3xl"
+                  animate={{
+                    rotate: activeItems[index] ? 180 : 0,
+                    color: activeItems[index] ? "#974ae8" : "#000000",
+                  }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <MdOutlineKeyboardArrowDown />
+                </motion.div>
               </motion.div>
+              
+              {/* answer */}
               <motion.div
-                className="text-3xl"
+                className={`text-start pt-[10px] font-semibold text-black/70 md:text-xl`}
+                initial={{ height: 0, opacity: 0 }}
                 animate={{
-                  rotate: activeItems[index] ? 180 : 0,
-                  color: activeItems[index] ? "#974ae8" : "#000000",
+                  height: activeItems[index] ? "auto" : 0,
+                  opacity: activeItems[index] ? 1 : 0,
+                  x: activeItems[index] ? 10 : 0 
                 }}
                 transition={{ duration: 0.3 }}
               >
-                <MdOutlineKeyboardArrowDown />
+                {item.answer}
               </motion.div>
-            </motion.div>
-            
-            {/* answer */}
-            <motion.div
-              className={`text-start pt-[10px] font-semibold text-black/70 md:text-xl`}
-              initial={{ height: 0, opacity: 0 }}
-              animate={{
-                height: activeItems[index] ? "auto" : 0,
-                opacity: activeItems[index] ? 1 : 0,
-                x: activeItems[index] ? 10 : 0 
-              }}
-              transition={{ duration: 0.3 }}
-            >
-              {item.answer}
-            </motion.div>
-          </button>
-        ))}
-      </div>
-    </section>
+            </button>
+          ))}
+        </div>
+      </section>
+    </AnimatedSections>
   );
 };
 
